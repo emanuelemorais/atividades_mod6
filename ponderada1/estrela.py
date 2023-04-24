b62f352da12146fbf43fef08e1b3b2717360ef6e
Emanuele Morais <emanuele.lmorais@gmail.com>
2023-04-23 21:03:04 -0300

    comentarios no codigo

--- ponderada1/estrela.py
+++ ponderada1/estrela.py
@@ -1,18 +1,18 @@
-#!/usr/bin/env python3
+# Bibliotecas necessárias
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-from turtlesim.srv import SetPen
 
+#Classe que controla o movimento da tartaruga
 class TurtleController(Node):
     def __init__(self):
         super().__init__('turtle_controller')
-        self.publisher_ = self.create_publisher(Twist, 'turtle1/cmd_vel', 1)
-        self.timer_ = self.create_timer(0.1, self.move_turtle)
+        self.publisher_ = self.create_publisher(Twist, 'turtle1/cmd_vel', 1) #publica a mensagem no tópico 'turtle1/cmd_vel' a cada 0.1 segundos
+        self.timer_ = self.create_timer(0.1, self.move_turtle) #chama a função 'move_turtle' a cada 0.1 segundos
         self.twist_msg_ = Twist()
-        self.timer = 0
+        self.timer = 0 # 'Timer' que controla a duração do movimento da tartaruga
         
-          
+    #funções que ditam coordenadas de movimento da tartaruga
     def movimento1(self):
         self.twist_msg_.linear.y = 4.0
         self.twist_msg_.linear.x = 2.0
@@ -45,7 +45,7 @@ class TurtleController(Node):
         self.timer += 1
         
 
-
+    #função que controla ordem e duração dos movimentos
     def move_turtle(self):
         if (self.timer < 10):
             self.movimento1()
@@ -73,3 +73,4 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+

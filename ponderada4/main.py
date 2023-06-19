2526498d64a5b0d69e95feec874d740915db89d2
Emanuele Morais <emanuele.lmorais@gmail.com>
2023-06-19 01:30:36 -0300

    backend servindo front

--- ponderada4/main.py
+++ ponderada4/main.py
@@ -4,6 +4,7 @@ from supabase import create_client, Client
 from dotenv import load_dotenv
 import os
 from record import record
+from fastapi.responses import HTMLResponse, RedirectResponse
 
 load_dotenv()
 url = os.getenv('API_URL')
@@ -16,9 +17,24 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
+    return HTMLResponse(content=open("static/index.html", "r").read())
+
+
+@app.get("/record")
+def read():
     filename = record()
     supabase.storage.from_(bucket_name).upload(f'{filename}.mp4', f'./{filename}')
-    return {"Vídeo salvo com sucesso!"}
+    alert = """
+    <head>
+        <meta http-equiv="refresh" content="0.5;URL='http://127.0.0.1:8000/'" />
+    </head>
+    <script>
+        alert("Video salvo com sucesso!! :)")
+    </script>
+    """
+    
+    return HTMLResponse(content=alert)
+
 
 if __name__ == "__main__": 
     uvicorn.run(app, host="0.0.0.0", port=8000)
